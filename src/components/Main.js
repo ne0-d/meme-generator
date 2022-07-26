d4597340c630087ed2726a4980fb17b26287e758
ne0-d <dhruvbhati2001@gmail.com>
2022-07-26 21:11:15 +0530

    Created navbar component

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+export default function Main(){
+    return(
+        <main>
+            <div class="nav--header">
+                <img src="/images/trollFace.png" className="logo" />
+                <h2>Meme Generator</h2>
+            </div>
+            <h4>React Course - Project 3</h4>
+        </main>
+    )
+}
