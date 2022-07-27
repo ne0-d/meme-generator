8e36c22c6169bdc0fadce190f82ac43f15022060
ne0-d <dhruvbhati2001@gmail.com>
2022-07-27 18:19:20 +0530

    Implemented react states in main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,13 +2,23 @@ import React from "react"
 import memesData from "../memesData"
 let memes = memesData.data.memes;
 export default function Main(){
-    const [memeImage, setMemeImage] = React.useState("");
-     
+   
+    const [memeImage, setMemeImage] = React.useState({
+        topText:"", 
+        bottomText:"", 
+        randomImage:"https://i.imgflip.com/30b1gx.jpg"
+    });
+    
     function getMemeImage(){
-        let randomNum = Math.floor(Math.random() * (100)) ;
-        setMemeImage(memes[randomNum].url);
-        console.log(memeImage);
+        let randomNum = Math.floor(Math.random() * (100));
+        setMemeImage(prevMeme => {
+            return {...prevMeme,
+                    randomImage:memesData.data.memes[randomNum].url
+            }
+
+        });
     }
+   
     return(
         <main>
                 <form action="#" className="form">
@@ -16,7 +26,7 @@ export default function Main(){
                     <input className="form--input" type='text' placeholder="Last Text"/>
                     <button onClick={getMemeImage} className="form--button">Get a new meme image  🖼</button>
                 </form>
-                <img src={memeImage} className="meme--image"/>
+                <img src={memeImage.randomImage} className="meme--image"/>
             
         </main>
     )
