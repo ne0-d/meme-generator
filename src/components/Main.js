b1ea05049f53deb1a36e793fb23cc3258dcd9d78
ne0-d <dhruvbhati2001@gmail.com>
2022-07-27 15:23:13 +0530

    It can now load random images from data.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,22 @@
+import React from "react"
+import memesData from "../memesData"
+let memes = memesData.data.memes;
 export default function Main(){
+    const [memeImage, setMemeImage] = React.useState("");
+     
+    function getMemeImage(){
+        let randomNum = Math.floor(Math.random() * (100)) ;
+        setMemeImage(memes[randomNum].url);
+        console.log(memeImage);
+    }
     return(
         <main>
-                <form action="" className="form">
+                <form action="#" className="form">
                     <input className="form--input" type='text' placeholder="First Text"/>
                     <input className="form--input" type='text' placeholder="Last Text"/>
-                    <button className="form--button">Get a new meme image  🖼</button>
+                    <button onClick={getMemeImage} className="form--button">Get a new meme image  🖼</button>
                 </form>
+                <img src={memeImage} className="meme--image"/>
             
         </main>
     )
